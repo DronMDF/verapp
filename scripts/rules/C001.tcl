d1fccee52f03b54697a36b6d32e3d62b893e9be2
Andrey Valyaev <dron.valyaev@gmail.com>
2020-01-18 22:35:44 +0300

    Count only public nonctor methods

--- scripts/rules/C001.tcl
+++ scripts/rules/C001.tcl
@@ -3,34 +3,52 @@
 
 set methodsPerClass [getParameter "method-per-class" 3]
 
+set vtype {
+	public
+	protected
+	private
+}
+
 foreach f [getSourceFileNames] {
 	set state "global"
+	set visibility "none"
+	set method "regular"
 	foreach t [getTokens $f 1 0 -1 -1 {}] {
 		set lineNumber [lindex $t 1]
 		set token [lindex $t 3]
 		if {$state == "global" && $token == "class"} {
 			set state "class"
+		} elseif {$state == "class" && $token == "identifier"} {
+			set className [lindex $t 0]
 		} elseif {$state == "class" && $token == "leftbrace"} {
 			set state "inclass"
 			set level 0
 			set methods 0
+		} elseif {$state == "inclass" && $token == "identifier" && [lindex $t 0] == $className} {
+			set method "ctor"
+		} elseif {$state == "inclass" && $token == "semicolon"} {
+			set method "regular"
 		} elseif {$state == "inclass" && $token == "leftparen"} {
 			set state "args"
 			set alevel 0
-			incr methods
+			if {$visibility == "public" && $method == "regular"} {
+				incr methods
+			}
+		} elseif {$state == "inclass" && [lsearch $vtype $token] != -1} {
+			set visibility $token
 		} elseif {$state == "args" && $token == "leftparen"} {
 			incr alevel
 		} elseif {$state == "args" && $token == "rightparen"} {
 			if {$alevel == 0} {
 				set state "inclass"
 			} else {
-				decr alevel
+				incr alevel -1
 			}
 		} elseif {$state == "inclass" && $token == "rightbrace"} {
 			if {$level == 0} {
 				set state "class"
 				if {$methods > $methodsPerClass} {
-					report $f $lineNumber "Class can have max $methodsPerClass methods, but have $methods."
+					report $f $lineNumber "Class '$className' have $methods public method ($methodsPerClass max)."
 				}
 			} else {
 				decr level
